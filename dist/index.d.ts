d651028183ceb4d09991875072e0c30ad8e5b80d
arunraj20it <101582292+arunraj20it@users.noreply.github.com>
2025-08-30 14:48:37 +0530

    Add files via upload

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export declare function getImgBase64Size(base64: string, unit?: string): number;
+export declare function imgcruncher(base64: string, quality?: number, maxWidth?: number, maxHeight?: number): Promise<string>;
+//# sourceMappingURL=index.d.ts.map
